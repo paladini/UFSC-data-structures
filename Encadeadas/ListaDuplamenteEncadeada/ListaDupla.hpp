c5fb145f9e3f951cfb4740fa815b12f26309bde9
skyrios <epodesta158@gmail.com>
2014-09-26 19:17:18 -0300

    Lista Duplamente Encadeada 100%

--- Encadeadas/ListaDuplamenteEncadeada/ListaDupla.hpp
+++ Encadeadas/ListaDuplamenteEncadeada/ListaDupla.hpp
@@ -1,26 +1,23 @@
 /** Copyright[2014]<Emmanuel Podestá Junior>
 */
-#ifndef LISTADUPLA_HPP_
-#define LISTADUPLA_HPP_
 #include "ElementoDupla.hpp"
 #include "ExcecaoErroPosicao.hpp"
 #include "ExcecaoListaCheia.hpp"
 #include "ExcecaoListaVazia.hpp"
-#include <iostream>
-template <typename T>
-class ListaDupla{
+template<typename T>
+class ListaDupla {
  private:
- 		Elemento<T>* head;
- 		int size;
+        ElementoDuplo<T>* head;
+        int size;
 
- 		void verificaPosicaoInvalida(int pos) {
+        void verificaPosicaoInvalida(int pos) {
             if (pos > size || pos < 0) {
                 throw ExcecaoErroPosicao;
             }
         }
 
          bool verificaMemoriaCheia() {
-            Elemento<T> *novo = new Elemento<T>(0, NULL);
+            ElementoDuplo<T> *novo = new ElementoDuplo<T>(0, NULL, NULL);
             if (novo == NULL) {
                 throw ExcecaoListaCheia;
             }
@@ -28,148 +25,201 @@ class ListaDupla{
             return true;
         }
 
-
  public:
- 	ListaDupla(){
- 		head = NULL;
- 		size = 0;
- 	}
- 	~ListaDupla(){
- 		destroiLista();
- 	}
-    void destroiLista(){
-        if(!listaVazia()){
-            Elemento<T>* eliminar;
-            while(head != NULL){
-                eliminar = head;
-                head = eliminar->getProximo();
-                delete eliminar;
-            }
-        } else {
+    ListaDupla() {
+        head = NULL;
+        size = 0;
+    }
+    ~ListaDupla() {
+        destroiListaDuplo();
+    }
+    void adicionaNoInicioDuplo(const T& dado) {
+        verificaMemoriaCheia();
+        ElementoDuplo<T>* novo = new ElementoDuplo<T>(dado, head, NULL);
+        head = novo;
+        if (novo->getProximo() != NULL) {
+            (novo->getProximo())->setAnterior(novo);
+        }
+        size++;
+    }
+    T retiraDoInicioDuplo() {
+        if (listaVazia()) {
             throw ExcecaoListaVazia;
         }
-    }
-    void adiciona(const T& dado){
-        adicionaNaPosicao(dado, size);
-    }
- 	void adicionaNoInicio(const T& dado){
- 		verificaMemoriaCheia();
- 		Elemento<T>* novo = new Elemento<T>(dado, head, NULL);
- 		head = novo;
- 		if(novo->getProximo() != NULL){
- 			(novo->getProximo())->setAnterior(novo);
- 		}
- 		size++;
- 	}
-    T retira(){
-        return retiraDaPosicao(size - 1);
-    }
- 	T retiraDoInicio(){
-        if(listaVazia()){
-            throw ExcecaoListaVazia;
-        }
-        Elemento<T>* saiu;
+        ElementoDuplo<T>* saiu;
         T volta;
         saiu = head;
         volta = saiu->getInfo();
         head = saiu->getProximo();
-        if(head != NULL){
-            head = head->setAnterior(NULL);
+        if (head != NULL) {
+            head->setAnterior(NULL);
         }
         size--;
         free(saiu);
         return volta;
     }
-    T retiraEspecifico(const T& dado){
-        int posicao = posicao(dado);
-        retiraDaPosicao(posicao);
+    void eliminaDoInicioDuplo() {
+        T elemento = retiraDoInicioDuplo();
+        delete elemento->getInfo();
+        delete elemento;
     }
-    void AdicionaNaPosicao(const T& dado, int pos){
-        Elemento<T>* novo = new Elemento<T>(dado, head, NULL);
-        Elemento<T>* anterior;
-        if(pos > size || pos < 0){
-            throw ExcecaoErroPosicao;
-        }
-        if(pos == 0){
-            adicionaNoInicio(dado);
-        } else {
-            verificaMemoriaCheia();
-            anterior = head;
-            for(int i = 0; i < pos - 1; i++){
-                anterior = anterior->getProximo();
-            }
-            novo->setProximo() = anterior->getProximo();
-            if(novo->getProximo() != NULL){
-                (novo->getProximo())->setAnterior(novo);
-            }
-            novo->setAnterior(anterior);
-            head->setProximo(novo);
-            size++;
-        }
-    }
-    T retiraDaPosicao(int pos){
+    void adicionaNaPosicaoDuplo(const T& dado, int pos) {
+        verificaMemoriaCheia();
+        ElementoDuplo<T>* novo = new ElementoDuplo<T>(dado, NULL, NULL);
+        ElementoDuplo<T>* anterior;
         if (pos > size || pos < 0) {
             throw ExcecaoErroPosicao;
         }
-        if(pos == 0){
-            retiraDoInicio();
+        if (pos == 0) {
+            adicionaNoInicioDuplo(dado);
+        } else {
+            anterior = head;
+            for (int i = 0; i < pos - 1; i++) {
+                anterior = anterior->getProximo();
+            }
+            novo->setProximo(anterior->getProximo());
+            if (novo->getProximo() != NULL) {
+                (novo->getProximo())->setAnterior(novo);
+            }
+            novo->setAnterior(anterior);
+            anterior->setProximo(novo);
+            size++;
         }
-        Elemento<T>* eliminar, anterior;
+    }
+    int posicaoDuplo(const T& dado) const {
+        if (listaVazia()) {
+            throw ExcecaoListaVazia;
+        } else {
+            ElementoDuplo<T> *atual = head;
+            for (int i = 0; i < size; i++) {
+               if (igual(atual->getInfo(), dado)) {
+                    return i;
+                }
+                atual = atual->getProximo();
+            }
+            throw ExcecaoErroPosicao;
+        }
+    }
+    T* posicaoMemDuplo(const T& dado) const {
+        if (listaVazia()) {
+            throw ExcecaoListaVazia;
+        } else {
+            ElementoDuplo<T> *atual = head;
+            for (int i = 0; i < size; i++) {
+                if (igual(atual->getInfo(), dado)) {
+                    return &atual->getInfo();
+                }
+                atual = atual->getProximo();
+            }
+        }
+        throw ExcecaoErroPosicao;
+    }
+    bool contemDuplo(const T& dado) {
+        try {
+            posicaoDuplo(dado);
+            return true;
+        } catch(std::exception& e) {
+            return false;
+        }
+    }
+    T retiraDaPosicaoDuplo(int pos) {
+        if (pos > size || pos < 0) {
+            throw ExcecaoErroPosicao;
+        }
+        if (pos == 0) {
+            return retiraDoInicioDuplo();
+        }
+        ElementoDuplo<T>* eliminar;
+        ElementoDuplo<T>* anterior = head;
         T volta;
-        anterior = head;
-        for(int i = 0; i < pos - 1; i++){
+        for (int i = 0; i < pos - 1; i++) {
             anterior = anterior->getProximo();
-        }    
+        }
         eliminar = anterior->getProximo();
         volta = eliminar->getInfo();
         anterior->setProximo(eliminar->getProximo());
-        if(eliminar->getProximo() != NULL){
+        if (eliminar->getProximo() != NULL) {
             (eliminar->getProximo())->setAnterior(anterior);
         }
         size--;
         delete eliminar;
         return volta;
     }
-    void adicionaEmOrdem(const T& dado){
-        verificaMemoriaCheia();
-        Elemento<T>* atual;
-        int posicao = 0;
-        atual = head;
-        while(atual != NULL && maior(dado, atual->getInfo())){
-            atual = atual->getProximo();
-            posicao++;
-        }
-        if(maior(dado, atual->getProximo())){
-            AdicionaNaPosicao(dado, posicao + 1);
+    void adicionaDuplo(const T& dado) {
+        adicionaNaPosicaoDuplo(dado, size);
+    }
+    T retiraDuplo() {
+        return retiraDaPosicaoDuplo(size - 1);
+    }
+    T retiraEspecificoDuplo(const T& dado) {
+        int posicao = posicaoDuplo(dado);
+        return retiraDaPosicaoDuplo(posicao);
+    }
+    T mostra(int pos) {
+        verificaPosicaoInvalida(pos);
+        if (listaVazia()) {
+            throw ExcecaoListaVazia;
         } else {
-            adicionaNaPosicao(dado, posicao);
+            ElementoDuplo<T> *atual = head;
+            if (pos == 0) {
+                return atual->getInfo();
+            }
+            for (int i = 0; i < pos; i++) {
+                atual = atual->getProximo();
+            }
+            return atual->getInfo();
         }
     }
-    bool listaVazia(){
+    void adicionaEmOrdem(const T& data) {
+        verificaMemoriaCheia();
+        ElementoDuplo<T>* atual;
+        int posicao;
+        if (listaVazia()) {
+            adicionaNoInicioDuplo(data);
+        } else {
+            atual = head;
+            posicao = 0;
+            while (atual->getProximo() != NULL && maior(data, atual->getInfo())) {
+                atual = atual->getProximo();
+                posicao++;
+            }
+            if (maior(data, atual->getInfo())) {
+                adicionaNaPosicaoDuplo(data, posicao + 1);
+            } else {
+                adicionaNaPosicaoDuplo(data, posicao);
+            }
+        }
+    }
+    int verUltimo() {
+        return size;
+    }
+    bool listaVazia() const {
         return size == 0;
     }
-    int posicao(const T& dado){
-        Elemento<T>* atual;
-        int posicao;
-        for(int i = 0; i < size; i++){
-            if(igual(dado, atual->getInfo())){
-                return i;
-            }
-            atual = atual->getProximo();
-            posicao++;
-        }
-        if(posicao >= size){
-            throw ExcecaoErroPosicao;
-        }
-    }
-    bool maior(const T& dado1, const T& dado2) const{
-        return dado1 > dado2;
-    }
-    bool menor(const T& dado1, const T& dado2) const{
-        return dado1 < dado2;
-    }
-    bool igual(const T& dado1, const T& dado2) const{
+    bool igual(T dado1, T dado2) const {
         return dado1 == dado2;
     }
+    bool maior(T dado1, T dado2) const {
+        return dado1 > dado2;
+    }
+    bool menor(T dado1, T dado2) const {
+        return dado1 < dado2;
+    }
+    void destroiListaDuplo(){
+       ElementoDuplo<T> *atual;
+        if (!listaVazia()) {
+            while (head != NULL) {
+                atual = head;
+                head = atual->getProximo();
+                delete atual;
+            }
+        } else {
+            delete head;
+        }
+        size = 0;
+    }
 };
+
+
+
 #endif

2c67f766b21e9c41082680490c4c37b9de491a9d
skyrios <epodesta158@gmail.com>
2014-09-29 17:05:28 -0300

    ListaDuplamenteEncadeada 100/100%

--- Encadeadas/ListaDuplamenteCircular/TesteCircDupla.cpp
+++ Encadeadas/ListaDuplamenteCircular/TesteCircDupla.cpp
@@ -1,9 +1,10 @@
-/*Copyright [2014] <Jean Martina>
+/*
+Copyright [2014] <Jean Martina>
 */
 #include <cstdlib>
-#include <ctime>
+
 #include "gtest/gtest.h"
-#include "ListaCircular.hpp"
+#include "ListaCircDupla.hpp"
 #define tam 10
 #define offset 1000
 
@@ -17,8 +18,8 @@ class Objeto{
 
 class TesteLista: public ::testing::Test{
  protected:
-	ListaCirc<int> l = ListaCirc<int>();
-	ListaCirc<Objeto*> lobj = ListaCirc<Objeto*>();
+	ListaDuplaCirc<int> l = ListaDuplaCirc<int>();
+	ListaDuplaCirc<Objeto*> lobj = ListaDuplaCirc<Objeto*>();
 };
 
 TEST_F(TesteLista, ListaVazia) {
@@ -27,82 +28,91 @@ TEST_F(TesteLista, ListaVazia) {
 }
 
 TEST_F(TesteLista, ListaVaziaNeg) {
-	l.adiciona(1);
+	l.adicionaDuplo(1);
 	ASSERT_FALSE(l.listaVazia());
-	lobj.adiciona(new Objeto);
+	lobj.adicionaDuplo(new Objeto);
 	ASSERT_FALSE(lobj.listaVazia());
 }
 
 TEST_F(TesteLista, ListaContem) {
 	Objeto* o = new Objeto();
-	l.adiciona(1);
-	ASSERT_TRUE(l.contem(1));
-	lobj.adiciona(o);
-	ASSERT_TRUE(lobj.contem(o));
+	l.adicionaDuplo(1);
+	ASSERT_TRUE(l.contemDuplo(1));
+	lobj.adicionaDuplo(o);
+	ASSERT_TRUE(lobj.contemDuplo(o));
 }
 
 TEST_F(TesteLista, ListaContemNeg) {
 	Objeto* o = new Objeto();
 	Objeto* o2 = new Objeto();
-	l.adiciona(1);
-	ASSERT_FALSE(l.contem(2));
-	lobj.adiciona(o);
-	ASSERT_FALSE(lobj.contem(o2));
+	l.adicionaDuplo(1);
+	ASSERT_FALSE(l.contemDuplo(2));
+	lobj.adicionaDuplo(o);
+	ASSERT_FALSE(lobj.contemDuplo(o2));
 }
 
 TEST_F(TesteLista, ListaDestroi) {
-	l.adiciona(1);
-	l.destroiLista();
+	l.adicionaDuplo(1);
+	l.destroiListaDuplo();
 	ASSERT_TRUE(l.listaVazia());
 }
 
 TEST_F(TesteLista, ListaPosicao) {
 	int i;
 	for (i = 0; i < tam ; i++) {
-	 l.adiciona(i);
+	 l.adicionaDuplo(i);
 	}
-	ASSERT_EQ(l.posicao(5), 5);
+	ASSERT_EQ(l.posicaoDuplo(5), 5);
 }
 
 TEST_F(TesteLista, ListaPosicaoExcep) {
 	int i;
 	for (i = 0; i < tam ; i++) {
+	 l.adicionaDuplo(i);
+	}
+	ASSERT_ANY_THROW(l.posicaoDuplo(tam+2));
+}
+
+/*TEST_F(TesteLista, ListaAdiciona) {
+	int i;
+	for (i = 0; i < tam ; i++) {
 	 l.adiciona(i);
 	}
-	ASSERT_ANY_THROW(l.posicao(tam+2));
-}
+	ASSERT_ANY_THROW(l.adiciona(i+1));
+}*/
 
 TEST_F(TesteLista, ListaAdicionaInicio) {
 	int i;
 	for (i = 0; i < tam-1 ; i++) {
-	 l.adicionaNoInicio(i);
+	 l.adicionaNoInicioDuplo(i);
 	}
-	l.adicionaNoInicio(i+1);
-	ASSERT_EQ(l.posicao(i+1), 0);
+	l.adicionaNoInicioDuplo(i+1);
+	ASSERT_EQ(l.posicaoDuplo(i+1), 0);
 }
+
 
 TEST_F(TesteLista, ListaAdicionaPosicao) {
 	int i;
 	for (i = 0; i < tam ; i++) {
-	 l.adicionaNaPosicao(i, i);
+	 l.adicionaNaPosicaoDuplo(i, i);
 	}
-	ASSERT_EQ(l.posicao(i-1), i-1);
+	ASSERT_EQ(l.posicaoDuplo(i-1), i-1);
 }
 
 TEST_F(TesteLista, ListaAdicionaPosicaoExcep1) {
 	int i;
 	for (i = 0; i < tam ; i++) {
-	 l.adicionaNaPosicao(i, i);
+	 l.adicionaNaPosicaoDuplo(i, i);
 	}
-	ASSERT_ANY_THROW(l.adicionaNaPosicao(i+1, i+1));
+	ASSERT_ANY_THROW(l.adicionaNaPosicaoDuplo(i+1, i+1));
 }
 
 TEST_F(TesteLista, ListaAdicionaPosicaoExcep2) {
 	int i;
 	for (i = 0; i < tam ; i++) {
-	 l.adicionaNaPosicao(i, i);
+	 l.adicionaNaPosicaoDuplo(i, i);
 	}
-	ASSERT_ANY_THROW(l.adicionaNaPosicao(i+1, i+2));
+	ASSERT_ANY_THROW(l.adicionaNaPosicaoDuplo(i+1, i+2));
 }
 
 TEST_F(TesteLista, ListaAdicionaOrdem) {
@@ -110,72 +120,73 @@ TEST_F(TesteLista, ListaAdicionaOrdem) {
 	for (i = 0; i < tam ; i++) {
 	 l.adicionaEmOrdem(tam-i);
 	}
-	ASSERT_TRUE((l.posicao(1)< l.posicao(2)));
+	ASSERT_TRUE((l.posicaoDuplo(1)< l.posicaoDuplo(2)));
 }
+
 
 TEST_F(TesteLista, ListaRemove) {
 	int i;
 	for (i = 0; i < tam ; i++) {
-	 l.adiciona(i);
+	 l.adicionaDuplo(i);
 	}
-	ASSERT_EQ(l.retira(), i-1);
+	ASSERT_EQ(l.retiraDuplo(), i-1);
 }
 
 TEST_F(TesteLista, ListaRemoveExcep) {
-	ASSERT_ANY_THROW(l.retira());
+	ASSERT_ANY_THROW(l.retiraDuplo());
 }
 
 TEST_F(TesteLista, ListaRemoveInicio) {
 	int i;
 	for (i = 0; i < tam ; i++) {
-	 l.adiciona(i);
+	 l.adicionaDuplo(i);
 	}
-	ASSERT_EQ(l.retiraDoInicio(), 0);
+	ASSERT_EQ(l.retiraDoInicioDuplo(), 0);
 }
 
 TEST_F(TesteLista, ListaRemoveInicioExcep) {
-	ASSERT_ANY_THROW(l.retiraDoInicio());
+	ASSERT_ANY_THROW(l.retiraDoInicioDuplo());
 }
 
 TEST_F(TesteLista, ListaRemovePosicao) {
 	int i;
 	for (i = 0; i < tam ; i++) {
-	 l.adiciona(i);
+	 l.adicionaDuplo(i);
 	}
-	ASSERT_EQ(l.retiraDaPosicao(1), 1);
+	ASSERT_EQ(l.retiraDaPosicaoDuplo(1), 1);
 }
 
 TEST_F(TesteLista, ListaRemovePosicaoExcep1) {
 	int i;
 	for (i = 0; i < tam ; i++) {
-	 l.adiciona(i);
+	 l.adicionaDuplo(i);
 	}
-	ASSERT_ANY_THROW(l.retiraDaPosicao(tam+2));
+	ASSERT_ANY_THROW(l.retiraDaPosicaoDuplo(tam+2));
 }
 
 TEST_F(TesteLista, ListaRemovePosicaoExcep2) {
-	ASSERT_ANY_THROW(l.retiraDaPosicao(1));
+	ASSERT_ANY_THROW(l.retiraDaPosicaoDuplo(1));
 }
 
 TEST_F(TesteLista, ListaRemoveEspecifico) {
 	int i;
 	for (i = 0; i < tam ; i++) {
-	 l.adiciona(i);
+	 l.adicionaDuplo(i);
 	}
-	ASSERT_EQ(l.retiraEspecifico(3), 3);
+	ASSERT_EQ(l.retiraEspecificoDuplo(3), 3);
 }
 
 TEST_F(TesteLista, ListaRemoveEspecificoExcep1) {
 	int i;
 	for (i = 0; i < tam ; i++) {
-	 l.adiciona(i);
+	 l.adicionaDuplo(i);
 	}
-	l.retiraEspecifico(3);
-	ASSERT_ANY_THROW(l.retiraEspecifico(3));
+	l.retiraEspecificoDuplo(3);
+	ASSERT_ANY_THROW(l.retiraEspecificoDuplo(3));
 }
 
 TEST_F(TesteLista, ListaRemoveEspecificoExcep2) {
-	ASSERT_ANY_THROW(l.retiraEspecifico(3));
+	ASSERT_ANY_THROW(l.retiraEspecificoDuplo(3));
 }
 
 TEST_F(TesteLista, Maior) {
@@ -201,4 +212,3 @@ TEST_F(TesteLista, Igual) {
 TEST_F(TesteLista, IgualNeg) {
 	ASSERT_FALSE(l.igual(2, 1));
 }
-

79de3b5f0aa1272eaf0ad6956062f463771e9533
skyrios <epodesta158@gmail.com>
2014-10-04 13:57:51 -0300

    Trabalho de implementação atualizado

--- TrabalhoDeImplementação/Pista.hpp
+++ TrabalhoDeImplementação/Pista.hpp
@@ -15,15 +15,35 @@ public:
 	Pista(int tam, int _velocidadeMedia): Fila<Carro>(tam) {
 		velocidadeMedia = _velocidadeMedia;
 		espacoOcupado = 0;
+		tamanho = tam;
 		// caminho = new Lista<Pista>(3);
 	}
 	void adicionaCarro(Carro c) {
-		if (espacoOcupado >= tamanho) {
-			throw -1;
+		int espacoOcupadoComCarro = espacoOcupado + c->getTamanho();
+		if (espacoOcupadoComCarro > tamanho) {
+			throw -1; // Excecao pista cheia / bloqueada
 		}
 		this->inclui(c);
-		espacoOcupado = c->getTamanho();
+		espacoOcupado = espacoOcupadoComCarro;
 	}
+
+	void estaCheia() {
+		// 5 por causa que 2m de trás, 1m de frente e 2 tamanho mínimo do carro
+		return espacoOcupado + 5 >= tamanho; 
+	}
+
+	/** Método estaBloqueada().
+	* Esse método verifica se é possível adicionar um carro de tamanho mínimo na pista, caso
+	* não seja possível significa que a pista deve ficar bloqueada, pois já está cheia. 
+	*
+	* O cálculo é feito baseado no espaço mínimo que pode ser ocupado por um carro em uma pista:
+	* 	espacoOcupado (até agora) + 3m (1m de frente, 2m de trás) + 2m (tamanho mínimo de um carro);
+	* 
+	* @return um boolean que, se true, indica se a pista está cheia.
+	*/
+	// bool estaBloqueada() {
+	// 	return espacoOcupado + 3 + 2 >= tamanho;
+	// }
 	// void setPistaEsquerda(*Pista p){
 	// 	caminho[0] = p;
 	// }
--- TrabalhoDeImplementação/Semaforo.hpp
+++ TrabalhoDeImplementação/Semaforo.hpp
@@ -1,13 +1,29 @@
 class Semaforo {
  private:
-	Lista<PistaFonte> *aferente;
-	Lista<PistaSumidouro> *eferente;
-	int luz;
+	Lista<Pista*> Pistas;
+	bool luz;
  public:
-	Semaforo(eferente, aferente) {
-		eferente = eferente;
-		aferente = aferente;
+	Semaforo(Pista* pistaAtual,Pista* pistaDireita, Pista* pistaEsquerda, Pista* pistaCentro/*,_luz*/) {
+		// luz = _luz
+ 		// luz = true//false;
+		Pistas = new Lista(3);
+		Pistas.adiciona(PistaAtual);
+		Pistas.adiciona(PistaDireita);
+		Pistas.adiciona(PistaCentro);
+		Pistas.adiciona(PistaEsquerda);
 	}
-
+	void setLuz(bool novaLuz) {
+		luz = novaLuz;
+	}
+	void passaCarro(Pista novaPista) {
+		if(luz) {
+			if(novaPista->estaCheia()) {
+				Carro c = (Pistas->mostra(0))->retira();
+				(Pistas->mostra(novaPista))->adicionaCarro(c);		
+			}
+		} else {
+			//fila bloqueada
+		}
+	}
 
 }

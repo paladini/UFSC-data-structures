8dbb23a8fdecbd3ab8069bf30a76acdb8bb46975
skyrios <epodesta158@gmail.com>
2014-10-08 16:04:58 -0300

    DOcumentado a classe carro

--- TrabalhoDeImplementação/Carro.hpp
+++ TrabalhoDeImplementação/Carro.hpp
@@ -3,22 +3,28 @@
 #include <cstdlib>
 #include <cstdio>
 
-// NAO MEXER NESSA CLASSE
-
 class Carro {
  private:
  		int tamanho, probabilidade;
  		
  public:
+ 		/* Construtor Carro
+ 		* COnstruirá um carro com um tamanho randômico entre 2 e 6 e calculará uma valor
+ 		* randômico entre 1 e 100 para posterior cálculo de probabilidade.
+ 		*/	
  		Carro() {
  			tamanho = (2 + (rand() % (int)(6 - 2 + 1))) + 3;
- 			probabilidade = (rand() % 10) + 1;
+ 			probabilidade = (rand() % 100) + 1;
   		}
-
+  		/* Método getProbabilidade
+  		* Retornará a probabilidade do carro.
+  		*/
  		int getProbabilidade() {
  			return probabilidade;
  		}
-
+ 		/* Método getTamanho
+ 		* Retornará o tamanho do carro.
+ 		*/
  		int getTamanho(){
  			return tamanho;
  		}
